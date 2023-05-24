105a2daef108419aeef98abc62ea64b7eee438ef
yuanliangliang01 <123069329+yuanliangliang01@users.noreply.github.com>
2023-05-24 12:00:15 +0800

    11

---
+++ lianxi/study.py
@@ -0,0 +1,11 @@
+import pymysql
+import pandas
+with pandas.ExcelFile('study_data.xls') as xls:
+
+      a=pandas
+conn= pymysql.connect(host="43.142.123.148", user="abc_mysql", password="12345678a", database="abc_mysql")
+
+
+#iterrows(): 按行遍历，将DataFrame的每一行迭代为(index, Series)对，可以通过row[name]对元素进行访问
+
+
